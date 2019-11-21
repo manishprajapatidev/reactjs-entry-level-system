942db80f7c096f3977f70d18f0ab0aa895f652bd
Manish Prajapati <manishprajapat41@gmail.com>
2019-11-22 02:20:12 +0530

    build

---
+++ build/precache-manifest.d2798fe62c0a3ee1d0dac23d8d699bec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc14e2a68cbab19d159eaddd3f1e7b5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bd9a6d624b526d40fa7",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "6ba6e25bbf7f849e0c98",
+    "url": "/static/css/main.be608b88.chunk.css"
+  },
+  {
+    "revision": "0bd9a6d624b526d40fa7",
+    "url": "/static/js/2.3e091052.chunk.js"
+  },
+  {
+    "revision": "a86c961e15f3ff6bbef3",
+    "url": "/static/js/3.59fdd7fb.chunk.js"
+  },
+  {
+    "revision": "6ba6e25bbf7f849e0c98",
+    "url": "/static/js/main.6afe85d4.chunk.js"
+  },
+  {
+    "revision": "78a64d2bd24802f51231",
+    "url": "/static/js/runtime-main.99683cbc.js"
+  },
+  {
+    "revision": "4544738faad70b96f2245dbf5ad532f6",
+    "url": "/static/media/visitors.4544738f.png"
+  }
+]);
